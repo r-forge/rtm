1b8f3200f8a639d13a90de3fc01d1fe621121ee4
slycoder (Jonathan Chang) <slycoder@gmail.com>
2011-10-09 09:51:58 +0000

    corpus
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rtm@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/onload.R
+++ pkg/R/onload.R
@@ -1,10 +1,4 @@
-NAMESPACE <- environment()
-.module <- new("Module")
-
 .onLoad <- function(libname, pkgname) {
-  require(methods)
-  unlockBinding(".module", NAMESPACE)
-  assign(".module", Rcpp:::Module("rtm"), NAMESPACE)
-  lockBinding(".module", NAMESPACE)
+  loadRcppModules()
 }
 
